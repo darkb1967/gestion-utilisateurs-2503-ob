7440f02eaab2f3a34aabf326c5bf00df3a6448d2
Olivier1967 <106076147+Olivier1967@users.noreply.github.com>
2025-08-07 15:57:49 +0200

    2ieme commit progression en cours

--- src/js/main.js
+++ src/js/main.js
@@ -11,26 +11,28 @@ const descrip = document.getElementById('descrip');
 // variable envois résultat dans une variable
 const result = document.getElementById('result');
 
+// table json dans fichier js
 const jsonData =[
-    {   "Utilisateur":"Invité",
-        "Role":["sidentifier", "créer un compte si code", "réinitialiser mot de passe"]
+    {   "Role":"Invité",
+        "Description":["sidentifier", "créer un compte si code", "réinitialiser mot de passe"]
     },
-    {   "Utilisateur":"Usager",
-        "Role":["consulter son profil", "modifier son profil", "parcourir la liste des utilisateurs","se déconnecter"]
+    {   "Role":"Usager",
+        "Description":["consulter son profil", "modifier son profil", "parcourir la liste des utilisateurs","se déconnecter"]
     },
-    {   "Utilisateur":"Encadrant",
-        "Role":["usager avec privilèges accrus", "consulter profils usagers et encadrants", "modifie infos usagers sauf rôle et mot de passe"]
+    {   "Role":"Encadrant",
+        "Description":["usager avec privilèges accrus", "consulter profils usagers et encadrants", "modifie infos usagers sauf rôle et mot de passe"]
     },
-    {   "Utilisateur":"Administrateur",
-        "Role":["encadrant avec privilèges accrus", "consulte profil tous utilisateurs", "modifie infos tous utilisateurs enregistré","ajoute utilisateur sans code inscription","supprimer un utilisateur"]
+    {   "Role":"Administrateur",
+        "Description":["encadrant avec privilèges accrus", "consulte profil tous utilisateurs", "modifie infos tous utilisateurs enregistré","ajoute utilisateur sans code inscription","supprimer un utilisateur"]
     }
 
 ];
 
 /* lancement des fonction de JS */
-//createFooter();
+//createFooter(); // a voir avec export
+//tableUser(); 
+
 tableJson(); // utilisation tableau Json
-tableUser(); // utilisation tableau utilisateur
 
 /* toutes les fonctions du JS */
 
@@ -40,13 +42,13 @@ function tableJson(){
     jsonData.forEach(utrole => {
     const row=document.createElement('tr');
 
-    const utilCell=document.createElement('td');
-    utilCell.textContent= utrole.Utilisateur;
-    row.appendChild(utilCell);
-
     const roleCell=document.createElement('td');
-    roleCell.textContent= utrole.Role.join(", ");
+    roleCell.textContent= utrole.Role;
     row.appendChild(roleCell);
+
+    const descripCell=document.createElement('td');
+    descripCell.textContent= utrole.Description.join(", ");
+    row.appendChild(descripCell);
 
     tableBody.appendChild(row);
 });
@@ -54,31 +56,110 @@ function tableJson(){
 }
 
 
-// fonction d'ajout d'un role, pas inplémenter encore
-function addRole() {
+// fonction d'ajout d'un role
+function afficherListePageRole() {
+    
+    //reset la liste
+    result.innerHTML = '';
 
-  // récupérer la valeur du champ input
-  let newRoleValue = nrole.value;
-  console.log(newRoleValue);
+    // fetch le fichier role dans data : déf->la promise, renvoyée par fetch, se résout avec un objet de la classe intégrée Response, dès que le serveur répond avec des en-têtes.
+    fetch('./data/role.json')
+    .then(res => res.json())
+    .then(role => {
 
-  let newDescripValue = descrip.value;
-  
-  jsonData["Utilisateur"]=newRoleValue;
-  jsonData.Utilisateur=newRoleValue;
+        //affiche la liste
+        role.forEach(post => {
+        //result.insertAdjacentHTML('beforeend', `<li>Role : ${post.Role} Description : ${post.Description}</li>`)
+        result.insertAdjacentHTML( 'beforeend',"<li>" + post.Role + " : " + post.Description + " </li>");
+        });
 
-  JSON.stringify["utilisateur", newRoleValue]
-  //jsonData.Role=newDescripValue;
+    });
 
-   result.append(JSON.stringify(jsonData));
-
-  /*let verifLetter = new RegExp("^[a-zA-Z]*$","g");
-  let verifLetter = new RegExp("^[a-zA-Z]*$");
-  if (!verifLetter.nrole || newRoleValue === " "){
-    alert("que des lettres s'il vous plaît !")
-  }
-  */
-
-
-
-  //refreshUi();
 }
+
+function boutonAjouterRoleDescrip(){
+
+    // récupérer la valeur du champ input
+    let newRoleValue = nrole.value.trim();
+    let newDescripValue = descrip.value.trim();
+
+    //let verifLetter = new RegExp("^[a-zA-Z]*$","g");
+    //let verifLetter = new RegExp("^[a-zA-Z]*$");
+    /*if (!verifLetter.nrole.value){
+        alert("que des lettres s'il vous plaît !")
+    }else{
+        alert("C'est bon !")
+    }*/
+    
+
+    fetch('./data/role.json')
+    .then(res => res.json())
+    .then(role => {
+
+    if (newRoleValue && newDescripValue) {
+            // Crée un nouvel objet avec le rôle et la description
+            const roletemp = { Role: newRoleValue, Description: newDescripValue };
+
+            // Ajoute l'objet au tableau 'role'
+            role.push(roletemp);
+
+            // Affiche temporairement le nouvel utilisateur avec rôle et description
+            result.insertAdjacentHTML('beforeend', `<li>Role: ${newRoleValue}, Description: ${newDescripValue}</li>`);
+
+            // Réinitialise les champs de saisie
+            nrole.value = '';
+            inputDescription.value = '';
+        } else {
+            alert("Veuillez remplir le rôle et la description.");
+        }  
+
+    });
+   
+}
+
+function addUtil(){
+
+    //reset la liste
+    result.innerHTML = '';
+
+    // fetch le fichier utilisateur dans data
+    fetch('./data/utilisateurs.json')
+    .then(res => res.json())
+    .then(utilisateurs => {
+
+        /*
+        //affiche la liste
+        utilisateurs.forEach(post => {
+        //result.insertAdjacentHTML('beforeend', `<li>Role : ${post.Role} Description : ${post.Description}</li>`)
+        result.insertAdjacentHTML( 'beforeend',"<li> Nom : " + post.Nom + " </li>");
+        result.insertAdjacentHTML( 'beforeend',"<li> Prénom : " + post.Prenom + " </li>");
+        result.insertAdjacentHTML( 'beforeend',"<li> Email : " + post.Email + " </li>");
+        result.insertAdjacentHTML( 'beforeend',"<li> Role : " + post.Role + " </li>");
+        });
+
+        */
+
+
+        // variable d'ajout role et sa description
+        const inputNom = document.getElementById('fname');
+        const inputPrenom = document.getElementById('lname');
+        const inputEmail = document.getElementById('email');
+        const inputRole = document.getElementById('role');
+
+        let champNom = inputNom.value.trim();
+        let champPrenom = inputPrenom.value.trim();
+        let champEmail = inputEmail.value.trim();
+        let champRole = inputRole.value.trim();
+
+        let addUtiltemp = { Nom: champNom, Prenom: champPrenom, Email: champEmail, Role: champRole };
+        console.log(addUtiltemp);
+        
+        // Ajoute l'objet au tableau 'data'
+        utilisateurs.push(addUtiltemp);
+
+        result.insertAdjacentHTML( 'beforeend',"<li> Nom : " + utilisateurs.Nom + " </li>");
+
+    });
+
+}
+
--- src/js/utilJson.js
+++ src/js/utilJson.js
@@ -1,57 +1,35 @@
 const tableUtBody = document.querySelector("#tableUtBody tbody");
 
-const jsonData =[
-    {   "Nom":"Jackson",
-        "Prenom":"makkenzie",
-        "Email":"makkenzie@gmail.com",
-        "Role":"invite"
-    },
-    {    "Nom":"Goya",
-        "Prenom":"Chantal",
-        "Email":"Chantalgoay@free.com",
-        "Role":"usager"
-    },
-    {    "Nom":"Pivot",
-        "Prenom":"Bernard",
-        "Email":"pbernard@hotmail.com",
-        "Role":"consultant"
-    },
-    {    "Nom":"Gate",
-        "Prenom":"Bill",
-        "Email":"billgate@laposte.fr",
-        "Role":"Administrateur"
-    }
-
-];
-
 /* lancement des fonction de JS */
-tableUtJson(); // utilisation tableau utilisateur
+tableauUtilisateurs();
 
 /* toutes les fonctions du JS */
+function tableauUtilisateurs(){
+    fetch('./data/utilisateurs.json')
+    .then(res => res.json())
+    .then(utilisateurs => {
 
-/* créate tableauJson */
-function tableUtJson(){
+        utilisateurs.forEach(util => {
+        const row=document.createElement('tr');
 
-    jsonData.forEach(utilisateurs => {
-    const row=document.createElement('tr');
+        const nomCell=document.createElement('td');
+        nomCell.textContent= util.Nom;
+        console.log(nomCell);
+        row.appendChild(nomCell);
 
-    const nomCell=document.createElement('td');
-    nomCell.textContent= utilisateurs.Nom;
-    row.appendChild(nomCell);
+        const prenomCell=document.createElement('td');
+        prenomCell.textContent= util.Prenom;
+        row.appendChild(prenomCell);
 
-    const prenomCell=document.createElement('td');
-    prenomCell.textContent= utilisateurs.Prenom;
-    row.appendChild(prenomCell);
+        const emailCell=document.createElement('td');
+        emailCell.textContent= util.Email;
+        row.appendChild(emailCell);
 
-    const emailCell=document.createElement('td');
-    emailCell.textContent= utilisateurs.Email;
-    row.appendChild(emailCell);
+        const roleCell=document.createElement('td');
+        roleCell.textContent= util.Role;
+        row.appendChild(roleCell);
 
-    const roleCell=document.createElement('td');
-    roleCell.textContent= utilisateurs.Role;
-    row.appendChild(roleCell);
-
-    tableUtBody.appendChild(row);
+        tableUtBody.appendChild(row);
+        });
     });
-
 }
